ff545531e5961e29a9a8015130d448b2a0da2a54
Nelson Laurensius <nelsonlaurensius1234@gmail.com>
2024-06-11 19:57:04 +0700

    First Commit

--- Tugas3_GroupC_ProgLan01.c
+++ Tugas3_GroupC_ProgLan01.c
@@ -1,65 +1,20 @@
+// Gerardin Nabil Zulhian 2306250661
+// Nelson Laurensius 2306161845
+// Neyla Shakira 2306250655
+// Nugroho Ulil Abshar 2306229310
+// Rafi Naufal Aryaputra 2306250680
+// Syahmi Hamdani  2306220532
+
+#include <omp.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "function.h"
-#include <omp.h>
+#include "Tugas3_GroupC_ProgLan01.h"
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-
-Akun akun[MAX_ACCOUNTS];
-int urutValid;
-
-void displayAwal(){
-    int inputAwal;
-    printf(ANSI_COLOR_YELLOW "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_RED "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_MAGENTA "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_GREEN "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_YELLOW " PROYEK AKHIR KELOMPOK C " ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_BLUE "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_MAGENTA "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_RED "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_GREEN "====\n" ANSI_COLOR_RESET);
-    printf("1. Konversi Suhu\n");
-    printf("2. Perkalian 1000\n");
-    printf("3. Exit\n");
-    printf("Masukkan Pilihan: ");
-    scanf("%d", &inputAwal);
-    switch (inputAwal){
-        case 1:
-            sesiGratis();
-            break;
-        case 2:
-            perkalian();
-            break;
-        case 3:
-            return 0;
-            break;
-        default:
-            break;
-    }
-}
-
-void sesiGratis(){
-    // Menampilkan sesi gratis
-    for (int i = 0; i < 3; i++) {
-        system("cls");
-        printf(ANSI_COLOR_BLUE "\n================ Sesi Gratis =================\n" ANSI_COLOR_RESET);
-        konversiSuhu();
-        system("pause");
-        system("cls");
-    }
-}
-
-void perkalian(){
+void Perkalian(){
     int bil1, bil2, t1, t2, i = 0, sum = 0;
     
+
     do
     {
         if (i > 0)
@@ -91,248 +46,100 @@ void perkalian(){
 
 }
 
-void displayJudul() {
-    printf(ANSI_COLOR_YELLOW "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_RED "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_MAGENTA "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_GREEN "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_YELLOW " K" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_BLUE "O" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_MAGENTA "N" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_RED "V" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_GREEN "E" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_YELLOW "R" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_BLUE "S" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_MAGENTA "I" ANSI_COLOR_RESET);
-    printf(" ");
-    printf(ANSI_COLOR_RED "S" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_GREEN "U" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_YELLOW "H" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_BLUE "U" ANSI_COLOR_RESET);
-    printf(" ");
-    printf(ANSI_COLOR_BLUE "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_MAGENTA "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_RED "====" ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_GREEN "====\n" ANSI_COLOR_RESET);
-}
+void updatePoint(const char *filename, Akun *akun) {
+    FILE *file = fopen(filename, "w");
 
-void displayKonversi(){
-    printf("||Satuan Awal Suhu                          ||\n");
-    printf("||1. Celcius                                ||\n");
-    printf("||2. Reamur                                 ||\n");
-    printf("||3. Fahrenheit                             ||\n");
-    printf("||4. Kelvin                                 ||\n");
-    printf("||Masukkan 0 untuk kembali ke menu awal     ||\n");
-    printf(ANSI_COLOR_CYAN "Masukkan Pilihan: " ANSI_COLOR_RESET);
-}
+    fprintf(file, "%s %s %d\n", akun->nama,akun->password, akun->point);
 
-void konversiSuhu() {
-    int pilihan1, pilihan2;
-    float suhuAwal, suhuAkhir;
-    Suhu suhu[4];
-    suhu[0].suhuAwal = 0;
-    suhu[0].Ratio = 5;
-    suhu[1].suhuAwal = 0;
-    suhu[1].Ratio = 4;
-    suhu[2].suhuAwal = 32;
-    suhu[2].Ratio = 9;
-    suhu[3].suhuAwal = 273.15;
-    suhu[3].Ratio = 5;
-
-    displayJudul();
-    displayKonversi();
-    scanf("%d", &pilihan1);
-
-    if (pilihan1 == 0) {
-        displayAwal();
-    }
-    printf("\n==============================================\n");
-    displayKonversi();
-    scanf("%d", &pilihan2);
-    if(pilihan2 == 0) {
-        displayAwal();
-    }
-
-    printf("\nMasukkan Suhu Awal: ");
-    scanf("%f", &suhuAwal);
-    
-    suhuAkhir = (suhuAwal - suhu[pilihan1-1].suhuAwal) * suhu[pilihan2-1].Ratio / suhu[pilihan1-1].Ratio + suhu[pilihan2-1].suhuAwal;
-
-    printf("\nSuhu Akhir: %0.2f\n", suhuAkhir);
-    system("pause");
-    system("cls");
-}
-
-void registrasiAkun(int *jumlahAkun) {
-    char nama[100], password[100];
-    FILE *file = fopen("akun.txt", "a");
-    if (file == NULL) {
-        printf(ANSI_COLOR_RED "Error opening file!\n" ANSI_COLOR_RESET);
-        return;
-    }
-
-    printf("Masukkan Nama Akun: ");
-    scanf("%s", nama);
-    printf("Masukkan Password: ");
-    scanf("%s", password);
-
-    for (int i = 0; i < *jumlahAkun; i++) {
-        if (strcmp(nama, akun[i].nama) == 0) {
-            printf(ANSI_COLOR_RED "Akun sudah terdaftar!\n" ANSI_COLOR_RESET);
-            system("pause");
-            fclose(file);
-            return;
-        }
-    }
-
-    fprintf(file, "%s %s %d\n", nama, password, MAX_CREDITS);
     fclose(file);
-    printf(ANSI_COLOR_GREEN "Registrasi berhasil!\n" ANSI_COLOR_RESET);
-    system("pause");
-    strcpy(akun[*jumlahAkun].nama, nama);
-    strcpy(akun[*jumlahAkun].password, password);
-    akun[*jumlahAkun].credits = MAX_CREDITS;
-    (*jumlahAkun)++;
-    system("pause");
-    system("cls");
 }
 
-int loginAkun(int jumlahAkun) {
-    char nama[100], password[100];
-    printf(ANSI_COLOR_YELLOW "Username: " ANSI_COLOR_RESET);
-    scanf("%s", nama);
-    printf(ANSI_COLOR_YELLOW "Password: " ANSI_COLOR_RESET);
-    scanf("%s", password);
+int main(){
+    int input, pilihan, valid = 0, urutValid;
+    char nama[100], password[100],voucher[100];
 
-    for (int i = 0; i < jumlahAkun; i++) {
-        if (strcmp(nama, akun[i].nama) == 0 && strcmp(password, akun[i].password) == 0) {
-            system("cls");
-            printf(ANSI_COLOR_GREEN "Berhasil Masuk\n" ANSI_COLOR_RESET);
-            urutValid = i;
-            return 1;
-        }
-    }
-    printf(ANSI_COLOR_RED "Akun belum terdaftar atau password salah\n" ANSI_COLOR_RESET);
-    system("pause");
-    return 0;
-}
-
-void logoutAkun(int *valid) {
-    urutValid = 0;
-    printf(ANSI_COLOR_GREEN "Berhasil Logout\n" ANSI_COLOR_RESET);
-}
-
-void redeemVoucher(char voucher[], char VoucherDatabase[MAX_VOUCHERS][100]) {
-    for (int i = 0; i < MAX_VOUCHERS; i++) {
-        if (strcmp(voucher, VoucherDatabase[i]) == 0) {
-            akun[urutValid].credits++;
-            printf(ANSI_COLOR_GREEN "Voucher Valid. Kredit Anda sekarang: %d\n" ANSI_COLOR_RESET, akun[urutValid].credits);
-            return;
-        }
-    }
-    printf(ANSI_COLOR_RED "Voucher Tidak Valid\n" ANSI_COLOR_RESET);
-}
-
-int main() {
-    // Inisialisasi variabel
-    int input, valid = 0, jumlahAkun = 0;
-    char voucher[100];
-
-    // Membuka file akun dan membaca data akun
-    FILE *akunFile = fopen("akun.txt", "r");
-    if (akunFile != NULL) {
-        while (fscanf(akunFile, "%s %s %d", akun[jumlahAkun].nama, akun[jumlahAkun].password, &akun[jumlahAkun].credits) != EOF) {
-            jumlahAkun++;
-        }
-        fclose(akunFile);
-    }
-
-    // Database voucher
-    char VoucherDatabase[MAX_VOUCHERS][100] = { "SUHUGRATIS", "TEMPERATURGRATIS", "KONVERSIGRATIS"};
-
-    displayAwal();
-
-    // Loop sampai user berhasil login
-    do {
+    Akun *akun = (Akun*)malloc(sizeof(Akun));
+    
+    for (int i = 0; i < 3; i++)
+    {
+        printf("\n====================Sesi Gratis %d====================\n", i+1);
+        konversiSuhu();
+        system("pause");
         system("cls");
-        printf(ANSI_COLOR_BLUE "\n========== Sesi Gratis Sudah Habis ===========\n" ANSI_COLOR_RESET);
-        displayJudul();
-        printf(ANSI_COLOR_YELLOW "Pilihan:\n" ANSI_COLOR_RESET);
-        printf(ANSI_COLOR_YELLOW "1. Registrasi\n" ANSI_COLOR_RESET);
-        printf(ANSI_COLOR_YELLOW "2. Login\n" ANSI_COLOR_RESET);
-        printf(ANSI_COLOR_YELLOW "Masukkan 0 untuk kmebali ke menu awal\n" ANSI_COLOR_RESET);
-        printf(ANSI_COLOR_YELLOW "Masukkan Pilihan: " ANSI_COLOR_RESET);
+    }
+
+    MenuLogin(akun);
+    
+    do
+    {
+        printf("\n======================================================\n");
+        printf("||                  Menu Pilihan                     ||\n");
+        printf("======================================================\n");
+        printf("||1. Konversi Suhu                                   ||\n");
+        printf("||2. Perkalian Ribuan                                ||\n");
+        printf("||3. Buat & Input Voucher                            ||\n");
+        printf("||4. Profil Pengguna                                 ||\n");
+        printf("||0. Keluar                                          ||\n");
+        printf("======================================================\n");
+        printf("Masukkan Pilihan: ");
         scanf("%d", &input);
+            
+        switch (input)
+        {
+        case 0:
+            printf("Terima Kasih Sudah Menggunakan Aplikasi Kami.\n");
+            updatePoint("akun.txt", akun);
+            system("pause");
+            break;
+        case 1:
+            if(akun->point == 0){
+                printf("Silahkan Kembali Ke Menu Utama Karena Point Anda Tidak Mencukupi\n");
+                system("pause");
+            }else{
+                konversiSuhu();
+                akun->point--;
+                system("pause");
+                system("cls");
+            };
+            break;
         
-        // Proses input user
-        switch (input) {
-            case 1:
-                registrasiAkun(&jumlahAkun);
-                break;
-            case 2:
-                valid = loginAkun(jumlahAkun);
-                break;
-            default:
-                printf(ANSI_COLOR_RED "Harap input dengan benar\n" ANSI_COLOR_RESET);
-                break;
-        }
-    } while (valid == 0);
+        case 2:
+             if(akun->point == 0){
+                printf("Silahkan Kembali Ke Menu Utama Karena Point Anda Tidak Mencukupi\n");
+                system("pause");
+            }else{
+                Perkalian();
+                akun->point--;
+                system("pause");
+                system("cls");
+            };
+            break;
 
-    // Jika login gagal, keluar dari program
-    if (!valid) {
-        return 0; 
-    }
+        case 3:
+            topUpVoucher("voucher.txt");
+            system("pause");
+            system("cls");
 
-    // Menu utama
-    do {
-        displayJudul();
-        printf(ANSI_COLOR_GREEN "Jumlah Kredit Anda : %d\n" ANSI_COLOR_RESET, akun[urutValid].credits);
-        printf(ANSI_COLOR_CYAN "||Menu Pilihan                              ||\n" ANSI_COLOR_RESET);
-        printf(ANSI_COLOR_CYAN "----------------------------------------------\n" ANSI_COLOR_RESET);
-        displayKonversi();
-        scanf("%d", &input);
-        system("cls");
-                
-        // Proses input user
-        switch (input) {
-            case 0:
-                break;
-            case 1:
-                if (akun[urutValid].credits == 0) {
-                    printf(ANSI_COLOR_RED "Kredit Anda telah habis. Silakan masukkan kode voucher untuk menambah kredit.\n" ANSI_COLOR_RESET);
-                }
-                else {
-                    konversiSuhu();
-                    akun[urutValid].credits--;
-                }
-                break;
-            case 2:
-                perkalian();
-                break;
-            case 3:
-                printf(ANSI_COLOR_YELLOW "Masukkan Kode Voucher: " ANSI_COLOR_RESET);
-                scanf("%s", voucher);
-                redeemVoucher(voucher, VoucherDatabase);
-                break;
-            case 4:
-                logoutAkun(&valid);
-                break;
-            default:
-                printf(ANSI_COLOR_RED "Harap input dengan benar\n" ANSI_COLOR_RESET);
-                break;
+            printf("Masukkan Kode Voucher: ");
+            scanf("%s", voucher);
+            int data = redeemVoucher("voucher.txt",voucher);
+            if (data == 0)
+            {
+                printf("Kode voucher yang Anda Masukkan Salah\n");
+            } else{
+                printf("Kode yang anda masukkan berhasil\n");
+                akun->point += data;
+            }
+            break;
+
+        case 4: 
+            profil(*akun);
+            system("cls");
+            break;
+
+        default:
+            printf("Harap input dengan benar\n");
+            break;
         }
     } while (input != 0);
-
-    // Update file akun
-    FILE *akunUpdate = fopen("akun.txt", "w");
-    if (akunUpdate != NULL) {
-        for (int i = 0; i < jumlahAkun; i++) {
-            fprintf(akunUpdate, "%s %s %d\n", akun[i].nama, akun[i].password, akun[i].credits);
-        }
-        fclose(akunUpdate);
-    } else {
-        printf(ANSI_COLOR_RED "Error updating account file!\n" ANSI_COLOR_RESET);
-    }
-
-    return 0;
 }
---
+++ Tugas3_GroupC_ProgLan01.h
@@ -0,0 +1,230 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+typedef struct 
+{
+    char nama[100];
+    char password[100];
+    int point;
+}Akun;
+
+typedef struct 
+{
+    char Nama[100];
+    float suhuAwal;
+    int Ratio;
+}Suhu;
+
+
+void konversiSuhu(){
+    int pilihan1, pilihan2;
+    float suhuAwal, suhuAkhir;
+    Suhu suhu[4];
+    //celcius
+    suhu[0].suhuAwal = 0;
+    suhu[0].Ratio = 5;
+    strcpy(suhu[0].Nama,"Celcius");
+    //reamur
+    suhu[1].suhuAwal = 0;
+    suhu[1].Ratio = 4;
+    strcpy(suhu[1].Nama,"Reamur");
+    //farenheit
+    suhu[2].suhuAwal = 32;
+    suhu[2].Ratio = 9;
+    strcpy(suhu[2].Nama,"Farenheit");
+    //kelvin
+    suhu[3].suhuAwal = 273.15;
+    suhu[3].Ratio = 5;
+    strcpy(suhu[3].Nama,"Kelvin");
+    do
+    {
+        printf("=======================================================\n");
+        printf("||Satuan Awal Suhu                                   ||\n");
+        printf("=======================================================\n");
+        printf("||1. Celicus                                         ||\n");
+        printf("||2. Reamur                                          ||\n");
+        printf("||3. Farenheit                                       ||\n");
+        printf("||4. Kelvin                                          ||\n");
+        printf("=======================================================\n");
+        printf("Pilihan: ");
+        scanf("%d", &pilihan1);
+        system("cls");
+    } while (pilihan1 > 4 || pilihan1 < 1);
+    
+    do
+    {
+        printf("=======================================================\n");
+        printf("||Satuan Akhir Suhu                                  ||\n");
+        printf("=======================================================\n");
+        printf("||1. Celicus                                         ||\n");
+        printf("||2. Reamur                                          ||\n");
+        printf("||3. Farenheit                                       ||\n");
+        printf("||4. Kelvin                                          ||\n");
+        printf("=======================================================\n");
+        printf("Pilihan: ");
+        scanf("%d", &pilihan2);
+        system("cls");
+    } while (pilihan2 > 4 || pilihan2 < 1);
+    
+
+    printf("Masukkan Suhu Awal: ");
+    scanf("%f", &suhuAwal);
+    
+    suhuAkhir = suhuAkhir = (suhuAwal - suhu[pilihan1-1].suhuAwal) * suhu[pilihan2-1].Ratio / suhu[pilihan1-1].Ratio + suhu[pilihan2-1].suhuAwal;
+
+    printf("Suhu Akhir dari %0.2f %s menjadi %s = %0.2f\n",suhuAwal, suhu[pilihan1].Nama, suhu[pilihan2].Nama, suhuAkhir);
+}
+
+
+
+
+//function untuk login
+void Regis(const char *filename, Akun *akun){
+    FILE *fptr;
+    fptr = fopen(filename,"a");
+    int valid = 0, pointDatabase;
+    char namaInput[100], passwordInput[100], namaDatabase[100], passwordDatabase[100];
+
+    do
+    {
+        printf("\n=======================================================\n");
+        printf("||Registration Mode (Hanya satu kata perisian)       ||\n");
+        printf("=======================================================\n");
+        printf("Username: ");
+        scanf("%s", namaInput );
+        printf("Password: ");
+        scanf("%s", passwordInput);
+
+        while (fscanf(fptr, "%s %s %d", namaDatabase ,passwordDatabase, &pointDatabase)!=EOF)
+        {
+            if((strcmp(namaDatabase, namaInput) == 0)&&(strcmp(passwordDatabase, passwordInput)==0)){
+                printf("Username dan Password yang anda masukkan sudah digunakan. Mohon masukkan yang lain.");
+                valid = 1;
+            }
+        }
+    } while (valid == 1);
+    
+
+    fprintf(fptr, "%s %s %d\n", namaInput,passwordInput, 10);
+    fclose(fptr);
+    strcpy(akun->nama, namaInput);
+    strcpy(akun->password,passwordInput);
+    akun->point = 10;
+}
+
+//function untuk Regis
+void Login(Akun *akun, const char *filename){
+    int valid = 0, pointDatabase;
+    char namaInput[100], passwordInput[100], namaDatabase[100], passwordDatabase[100];
+    FILE *fptr;
+    fptr = fopen(filename, "r");
+
+    
+    printf("\n=======================================================\n");
+    printf("||Login Mode (Hanya satu kata perisian)              ||\n");
+    printf("=======================================================\n");
+    printf("Username: ");
+    scanf("%s", namaInput );
+    printf("Password: ");
+    scanf("%s", passwordInput);
+
+    while (fscanf(fptr, "%s %s %d", namaDatabase ,passwordDatabase, &pointDatabase)!=EOF)
+    {
+        if((strcmp(namaDatabase, namaInput) == 0)&&(strcmp(passwordDatabase, passwordInput)==0)){
+            strcpy(akun->nama,namaInput);
+            strcpy(akun->password,passwordInput);
+            akun->point = pointDatabase;
+            valid = 1;
+        }
+    }
+    fclose(fptr);
+
+    if (valid == 0)
+    {
+        printf("Nama yang Anda Masukkan Salah, Harap daftarkan akun anda");
+    }
+    
+}
+
+void MenuLogin(Akun *akun){
+    int MenuLogin;
+    do
+    {
+        printf("\n===============Sesi Gratis Sudah Habis===============\n");
+        printf("||1. Registrasi                                     ||\n");
+        printf("||2. Login                                          ||\n");
+        printf("=====================================================\n");
+        printf("Masukkan Pilihan: ");
+        scanf("%d", &MenuLogin);
+        system("cls");
+        if(MenuLogin == 1){
+            Regis("akun.txt",akun);
+            system("cls");
+        }else if(MenuLogin == 2){
+            Login(akun,"akun.txt");
+            system("cls");
+        }else{
+            printf("Masukkan Pilihan yang benar\n");
+        }
+    } while ((MenuLogin != 1) && (MenuLogin != 2));
+}
+
+
+void generateVoucher(char *code, int length) {
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    for (int i = 0; i < length; i++) {
+        int index = rand() % (int)(sizeof(charset) - 1);
+        code[i] = charset[index];
+    }
+
+    code[length] = '\0';
+}
+
+void topUpVoucher(const char *filename) {
+    char code[21];
+    generateVoucher(code, 20);
+    printf("Code Voucher: %s\n", code);
+
+    FILE *fptr;
+
+    fptr = fopen(filename,"a");
+    fprintf(fptr,"%s\n",code);
+    fclose(fptr);
+    printf("Voucher berhasil di buat. Silahkan Cek voucher.txt untuk kode voucher anda\n");
+}
+
+int redeemVoucher(const char *filename, const char *code) {
+    int data,valid=0;
+    char dataCode[21];
+    FILE *fptr;
+  
+    fptr = fopen(filename, "r");
+
+    while (fscanf(fptr, "%s", dataCode)!=EOF)
+    {
+        if(strcmp(dataCode, code) == 0){
+            printf("Voucher berhasil di reedem");
+            valid = 1;
+        }
+    }
+    
+    fptr = fopen(filename, "w+");
+    fclose(fptr);
+
+    if(valid == 1){
+        return 10;
+    }else{
+        return 0;
+    }
+        
+}
+
+void profil(Akun akun){
+    printf("Nama\t: %s\n",akun.nama);
+    printf("Password: %s\n",akun.password);
+    printf("Point\t: %d\n",akun.point);
+    system("pause");
+}
